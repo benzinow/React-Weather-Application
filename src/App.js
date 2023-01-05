58658c1dfda1a780923d3bd6c98e65e6bd48b192
benzinow <benzinow@gmail.com>
2023-01-04 22:42:30 -0700

    add githup repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <div>React Weather App</div>;
+  return (
+    <div>
+      React Weather App
+      <p>
+        This project was coded by Benzi Holler and is{" "}
+        <a
+          target="_blank"
+          href="https://github.com/benzinow/react-weather-application"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a target="_blank" href="https://grand-taiyaki-2e97f9.netlify.app/">
+          hosted on Netlify
+        </a>
+      </p>
+    </div>
+  );
 }
 
 export default App;
